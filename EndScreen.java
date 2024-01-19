e6f152b9b23b232d9be88b71a2c33405dcb91264
DylanDinesh <350444048@gapps.yrdsb.ca>
2024-01-18 23:44:36 -0500

    Additional Changes

--- EndScreen.java
+++ EndScreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class EndScreen extends World
 {
     GreenfootSound gameOverMusic = new GreenfootSound("lose.mp3");
-    
+
     public EndScreen(int score)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -17,11 +17,15 @@ public class EndScreen extends World
         setBackground("gameover.jpg");
         gameOverMusic.play();
         Label gameOver = new Label("GAME OVER", 40);
-        addObject(gameOver,300,344);
+        addObject(gameOver,300,244);
         Label  finalScore = new Label("Final Score is: " + score, 40);
-        addObject(finalScore,300,250);
+        addObject(finalScore,300,150);
         Label spaceToRetry = new Label("Press Space to retry....", 40);
-        addObject(spaceToRetry,300,300);    
+        addObject(spaceToRetry,300,200);    
+    }
+
+    public void act()
+    {
         if (Greenfoot.isKeyDown("space")) {
             gameOverMusic.pause();
             Trash.speed = 1;
