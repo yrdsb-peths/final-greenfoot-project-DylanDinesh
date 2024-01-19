5685e55ee7c2b066537f2376d6087cc2cb652f64
DylanDinesh <350444048@gapps.yrdsb.ca>
2024-01-19 13:28:32 -0500

    Changed front screen background

--- StartScreen.java
+++ StartScreen.java
@@ -19,6 +19,7 @@ public class StartScreen extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         prepare();
+        setBackground("start.png");
         openingSong.play();
         addObject(titleLabel, getWidth()/2, 40);
         prepare();
@@ -45,16 +46,16 @@ public class StartScreen extends World
     private void prepare()
     {
         Label instructionsOne = new Label("Move the turtle with your mouse. The turtle will go to the mouse.", 20);
-        instructionsOne.setLineColor(Color.RED);
+        instructionsOne.setLineColor(Color.WHITE);
         addObject(instructionsOne,300,304);
         Label instructionsTwo = new Label("Eat seagrass for points and go the the lightning for a speed power up.", 20);
-        instructionsTwo.setLineColor(Color.GREEN);
+        instructionsTwo.setLineColor(Color.WHITE);
         addObject(instructionsTwo,300,327);
         Label instructionsThree = new Label("Avoid the trash or else you lose!", 25);
-        instructionsThree.setLineColor(Color.GREEN);    
+        instructionsThree.setLineColor(Color.WHITE);    
         addObject(instructionsThree,300,350);
         Label spaceToStart = new Label("Click space to start...", 20);
-        spaceToStart.setLineColor(Color.GRAY);
+        spaceToStart.setLineColor(Color.WHITE);
         addObject(spaceToStart,300,380);
         Turtle turtle = new Turtle();
         addObject(turtle,getWidth()/2,250);
